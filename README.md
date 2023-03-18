1ead367cc3b6a202de4ddd3e768aaa37a4703d22
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-03-18 15:58:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Surgical segmentation models
+
+## Installation of `surg_seg`
+
+`surg_seg` is a python package that includes most of the code to interface with the trained models
+
+```bash
+pip install -e . -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="surg_seg",
+    version="0.0.0",
+    author="Juan Antonio Barragan",
+    author_email="jbarrag3@jh.edu",
+    description="Surgical segmentation models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "scipy", "rich", "click"],
+    include_package_data=True,
+    python_requires=">=3.8",
+)
